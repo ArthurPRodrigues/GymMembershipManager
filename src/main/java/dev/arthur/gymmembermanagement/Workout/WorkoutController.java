40291e1a5884319df3367b52a352336960e30f42
arthuPRodrigues <arthurprodrigues1@gmail.com>
2026-03-20 17:21:10 -0300

    feat: configure application properties and update controller mappings

--- src/main/java/dev/arthur/gymmembermanagement/Workout/WorkoutController.java
+++ src/main/java/dev/arthur/gymmembermanagement/Workout/WorkoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/workouts")
 public class WorkoutController {
 
 	@GetMapping("/welcome")
