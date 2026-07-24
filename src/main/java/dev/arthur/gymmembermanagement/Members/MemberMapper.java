2b13abbfcaf36c735080dbbb2dd62a3c80b25060
arthurPRodrigues <arthurprodrigues1@gmail.com>
2026-07-24 15:44:40 -0300

    fix: adjust memberMapper to translate DTO to Entity

--- src/main/java/dev/arthur/gymmembermanagement/Members/MemberMapper.java
+++ src/main/java/dev/arthur/gymmembermanagement/Members/MemberMapper.java
@@ -1,5 +1,8 @@
 package dev.arthur.gymmembermanagement.Members;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberMapper {
 
 	public MemberModel map(MemberDTO memberDTO) {
@@ -12,6 +15,18 @@ public class MemberMapper {
 		memberModel.setWorkouts(memberDTO.getWorkouts());
 
 		return memberModel;
+	}
+
+	public MemberDTO map(MemberModel memberModel) {
+		MemberDTO memberDTO = new MemberDTO();
+		memberDTO.setId(memberModel.getId());
+		memberDTO.setName(memberModel.getName());
+		memberDTO.setAge(memberModel.getAge());
+		memberDTO.setEmail(memberModel.getEmail());
+		memberDTO.setMembershipType(memberModel.getMembershipType());
+		memberDTO.setWorkouts(memberModel.getWorkouts());
+
+		return memberDTO;
 	}
 
 }
